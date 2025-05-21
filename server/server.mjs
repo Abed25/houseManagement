0e258127efdb567d34d728ba28dcf514bda86062
Abed25 <officialwekesa254@gmail.com>
2025-05-21 11:09:29 +0300

    Removed unnecessary mysql database import

--- server/server.mjs
+++ server/server.mjs
@@ -1,20 +1,10 @@
 import app from "./app.mjs";
 import connectDB from "./config/db.mjs";
-import db from "./config/database.mjs";
 
 const PORT = process.env.PORT || 5000;
 
 // Connect to database
 connectDB();
-
-db.getConnection((err, connection) => {
-  if (err) {
-    console.error("Database connection failed:", err.stack);
-    return;
-  }
-  if (connection) connection.release(); // Release the connection back to the pool
-  console.log("Connected to MySQL database pool");
-});
 
 app.get("/", (req, res) => {
   res.send("Backend all is set");
